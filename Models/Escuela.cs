034b56fdecb9487c142f23104b7aa60832a5fc0c
Francisco Ferreira de la cruz <ferreiradelacruz@hotmail.com>
2020-01-05 23:36:47 +0100

    primeros pasos

---
+++ Models/Escuela.cs
@@ -0,0 +1,9 @@
+namespace NetCorePlatzi.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId {get; set;}
+        public string Nombre {get; set;}
+        public int AnioFundacion {get; set;}
+    }
+}
